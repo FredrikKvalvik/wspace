27bff6b79360294c40de0e04bb5d7cb034594d39
Fredrik Kvalvik <fredrik.kvalvik@gmail.com>
2022-05-05 16:13:01 +0200

    add url validation function

--- src/utils.ts
+++ src/utils.ts
@@ -1,19 +1,29 @@
-import { exec } from "child_process"
+import { exec } from "child_process";
 
-import config from "../config.js"
-import { getWsData } from "./generateList.js"
+import config from "../config.js";
+import { getWsData } from "./generateList.js";
 
 export const findWorkSpace = (wpList, wpName) => {
-  return wpList.find(ws => ws.workPlace == wpName)
-}
+  return wpList.find((ws) => ws.workPlace == wpName);
+};
+
+export const validateUrl = (url): Boolean => {
+  let isValidUrl;
+  try {
+    isValidUrl = new Boolean(new URL(url));
+  } catch (err) {
+    isValidUrl = false;
+  }
+  return isValidUrl;
+};
 
 export const openEditor = (path) => {
-  exec(`${config.defaultEditor} ${path}`)
-}
+  exec(`${config.defaultEditor} ${path}`);
+};
 
 export const ls = () => {
-  const wsList = getWsData()
-  wsList.forEach(ws => {
-    console.log("-", ws.workSpace)
-  })
-}
+  const wsList = getWsData();
+  wsList.forEach((ws, i) => {
+    console.log(`${i} ${ws.workSpace}`);
+  });
+};
